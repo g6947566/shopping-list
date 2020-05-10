518624d95923e2f89f2f4448db8f0bde61fdd897
NL <NL@NLs-MacBook-Air.local>
2020-05-09 22:10:48 -0400

    added the delete and check function

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-$(function(){
+function userListForm(){
+
     $("#js-shopping-list-form").submit(function(event){
         event.preventDefault();
-        const userTextDisplay =$(event.currentTarget).find("#shopping-list-entry");
-        $('ul').append(`<li>`+ `<div class="shopping-item">${userTextDisplay.val()} </div>` +`<button><div class="shopping-item-toggle">check</div></button>` + " " + `<button><div class="shopping-item-delete">delete</div></button>`);
+        // console.log("submit works");
+        const userTextDisplay =$("#shopping-list-entry").val();
+        // console.log(userTextDisplay);
         
-
+        $('ul').append(`<li>`+ `<div class="shopping-item">${userTextDisplay} </div>` 
+        +`<button><div class="shopping-item-toggle">check</div></button>` + " " 
+        + `<button><div class="shopping-item-delete">delete</div></button>`);
+             
     });
 
-})
+    $(".shopping-list").on("click", ".shopping-item-delete", function(event) {
+        $(this).parent().parent().remove(); 
+     });
 
+    $(".shopping-list").on("click", ".shopping-item-toggle", function(event){
+         $(this).parent().parent().find(".shopping-item").toggleClass('shopping-item__checked');
+    });
+
+}
+
+
+$(userListForm);
 
