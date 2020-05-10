15ffa70e1cd740eced3a879116bbc6274bf11cfd
NL <NL@NLs-MacBook-Air.local>
2020-05-09 20:16:05 -0400

    created userTextDisplay output

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ $(function(){
     $("#js-shopping-list-form").submit(function(event){
         event.preventDefault();
         const userTextDisplay =$(event.currentTarget).find("#shopping-list-entry");
-        $('ul').append("<li>" + `user text is: ${userTextDisplay.val()}`);
-        // userTextDisplay.val("");
+        $('ul').append(`<li>`+ `<div class="shopping-item">${userTextDisplay.val()} </div>` +`<button><div class="shopping-item-toggle">check</div></button>` + " " + `<button><div class="shopping-item-delete">delete</div></button>`);
+        
 
     });
 
